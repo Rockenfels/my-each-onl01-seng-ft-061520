1192c24228e894b8e88a2b1d53c46a30398bccd9
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-18 14:06:47 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i = 0
+    while i < array.length do
+      i += 1
+      yield array[i]
+    end
+  else
+    puts "Invalid Argument!"
+  end
 end
